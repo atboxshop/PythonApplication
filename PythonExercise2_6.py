aaa7f2bc58a13111995945958234369f7706e88d
Dark Alex <trananhtai@hotmail.com>
2022-11-06 21:13:50 +0700

    Python Exersice 2.6

--- PythonExercise2_6.py
+++ PythonExercise2_6.py
@@ -1,1 +1,5 @@
-
+number = int(input('Enter any number you like to see if it is odd or even: '))
+if number % 2 == 0:
+    print('This is even number')
+if number % 2 != 0:
+    print('This is odd number')
