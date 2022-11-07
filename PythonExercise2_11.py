df0a304b186dadca053f743d4edddb229eaed411
Dark Alex <trananhtai@hotmail.com>
2022-11-07 11:48:27 +0700

    code changed

--- PythonExercise2_11.py
+++ PythonExercise2_11.py
@@ -1,14 +1,22 @@
+# Python Exercise 2.11
+
 print('Convert from seconds to hours and minutes program')
+
 seconds = int(input('Enter a number of seconds you want: '))
+
 hours = seconds // 3600
+
 minutes = seconds % 3600
+
 if minutes == 60:
     print(hours,'hours \
     ',minutes // 60,'minutes \
     and 0 seconds')
+
 if minutes < 60:
     print(hours,'hours \
    and',minutes, 'minutes')
+
 if minutes > 60:
     print(hours,'hours \
     ',minutes // 60, 'minutes \
