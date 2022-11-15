66aca19bf2f2d4bbc9188d5b346bc934209c692b
Dark Alex <trananhtai@hotmail.com>
2022-11-15 19:46:45 +0700

    Python Qt5

--- pyqt.py
+++ pyqt.py
@@ -6,10 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-
+from PyQt5 import QtCore, QtWidgets, QtGui
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
