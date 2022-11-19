98ed57445c7d0a9cd87004352e8eb46d60205554
Dark Alex <trananhtai@hotmail.com>
2022-11-19 17:59:26 +0700

    code changed

--- PythonExercise3_02.py
+++ PythonExercise3_02.py
@@ -3,28 +3,28 @@
 """Class average program with sentinel-controlled iteration"""
 
 # initialization phase
-from asyncio.windows_events import NULL
-
-
 total = 0 # sum if grades
 grade_counter = 0 # number of grades entered
 
 # processing phase
 grade = int(input('Enter grade, 0 to end: ')) # get one grade
 
-if grade == -1:
-    print('No grades were entered')
-elif grade < 0:
-    print('Grade must equal or greater than 0')
+if grade <= -1:
+    print('No grades were entered,', 
+          'grade must be equal or greater than 0')
 else:
     while grade != -1:
         total += grade
         grade_counter += 1
         grade = int(input('Enter grade, 0 to end: '))
+        if grade == -1:
+            grade = -1
         if grade < 0:
             print('Grade value must be run from 0 to end')
-            grade = -1
-    if grade != 0:
+            grade = 0
+            grade_counter -= 1
+
+    if grade != 0: #Termination proccess
         average = total / grade_counter
         print(f'Class average is {average:.2f}')
 
