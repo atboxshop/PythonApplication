2f0e278da730aebb3a8744fb61770be7def81cec
Dark Alex <trananhtai@hotmail.com>
2022-11-20 10:57:58 +0700

    changed

---
+++ Python Fig3_03.py
@@ -0,0 +1,19 @@
+passes = 0
+failures = 0
+
+for student in range[10]:
+    result = int(input('Enter result (1=pass, 2=fail):'))
+
+    if result == 1:
+        passes += 1
+    elif result == 2:
+        failures += 1
+    else:
+        print('Result inputing must be either 1 or 2')
+
+print('Passed:', passes)
+print('Failed:', failures)
+
+if passes > 8:
+    print('Bonus to instructor')
+
